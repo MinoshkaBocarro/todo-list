7922248f24737a7746545095e0d57285147ec13f
mb <115391907+FireHeart3198@users.noreply.github.com>
2024-04-09 10:44:29 +1000

    Add preliminary Project class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 class TodoItem {
-    constructor(title, description, dueDate) {
+    constructor({title, description, dueDate}) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         // add priority, repeated, notes and checklist
+    }
+}
+
+class Project {
+    itemList = [];
+
+    constructor(projectName){
+        this.projectName = projectName;
+    }
+
+    addItem(item) {
+        this.itemList.push(item);
     }
 }
